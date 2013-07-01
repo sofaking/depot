bbf79b7d012572bbb1df9010386504b2d400751c
Anton Petrov <givemeletter@gmail.com>
2013-07-01 13:47:27 +0400

    Ch. 9. Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    session[:counter] = 0 if session[:counter].nil?
+    session[:counter] += 1
   end
 end
